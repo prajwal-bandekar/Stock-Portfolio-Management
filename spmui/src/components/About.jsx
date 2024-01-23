0dfa4be0bf8db982c89877b2fce664bef0bf76c7
Prajwal <prajwalbandekar@gmail.com>
2024-01-23 18:43:38 +0530

    Completed Landing page about 75%

--- spmui/src/components/About.jsx
+++ spmui/src/components/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+   <>
+   InvestMitra helps you maximize your returns and grow your wealth 
+   through smart investment decisions. Whether you're new to investing
+    or an experienced investor, InvestMitra puts all the tools you need
+     to make wise investments quickly and easily at your fingertips.
+    Get started today and take your finances to the next level.
+  </>
   )
 }
 
--- spmui/src/components/LP.jsx
+++ spmui/src/components/LP.jsx
@@ -21,11 +21,14 @@ const LP = () => {
         </div>
 
         <div id="herosection">
-          <h1>Track Your Investments</h1>
-          <br />
+          <h1>Track Your </h1>
+          <br /> 
 
+          <h2 id="h2Invest">Investments</h2>
+          <br />
+        
           <h5>
-            Monitor your stock market portfolio in real-time with our intuitive
+            Monitor your stock market portfolio in real-time <br/>with our intuitive
             and stylish tracker.
           </h5>
           <br />
@@ -40,7 +43,7 @@ const LP = () => {
 
       <div id="parentdiv2">
         <div id="header">
-        <h1>Key Features</h1>
+        <h1><span id="KeySpan">Key</span> <span id="FeaturesSpan">Features</span> </h1>
         <h5>Our portfolio tracker offers a range of features designed to help you stay on top of your investments.</h5>
         </div>
         <div id="tab">
@@ -52,15 +55,86 @@ const LP = () => {
 
           <div className="tabchild">
             {/* ADD DIFFERENT ICONS */}
-          <i class="fa-regular fa-clipboard fa-2xl"></i>
+         <i class="fa-solid fa-magnifying-glass-chart fa-2xl"></i>
             <h1 className="h1tab">Detailed Analysis</h1>
-            <h5 className="h5tab">Access detailed analysis of your portfolio's <br/> performance.</h5></div>
+            <h5 className="h5tab">Access detailed analysis of <br/> your portfolio's  performance.</h5></div>
 
           <div className="tabchild">
             {/* ADD DIFFERENT ICONS */}
             <i class="fa-regular fa-bell fa-2xl"></i>
             <h1 className="h1tab">Custom Alerts</h1>
-            <h5 className="h5tab">Set custom alerts for your stocks and never <br/> miss a market move.</h5></div>
+            <h5 className="h5tab">Set custom alerts for your stocks  <br/>and never miss a market move.</h5></div>
+        </div>
+      </div>
+
+      {/* PAGE 3 */}
+      <div id="parentdiv3">
+      <div id="header">
+        <h1>Testi<span id="monials">monials</span></h1>
+        <h5>Don't just take our word for it. Hear what our users have to say.</h5>
+        </div>
+
+      <div id="tab-container">
+
+      <div className="tabs"> 
+      <img src="https://avatars.githubusercontent.com/u/8151169?v=4" alt="" />
+      <h1>Erlich Bachman</h1>
+      <p>"InvestMitra, the ultimate game changer for managing 
+            my stock portfolio, delivers real-time updates and custom alerts – hands down,
+            the best damn portfolio tracker  I've ever used. I've got skin in the tech game, 
+            owning a solid 10% of Pied Piper. And let's not forget, I have made strategic 
+            moves like selling Aviato in the end. Highly recommend it because missing a 
+            market move is so last season!"</p>
+      <p></p>
+      </div>
+
+      <div className="tabs">
+      <img src="https://pbs.twimg.com/profile_images/731951677859868674/2iRz4Ikp_400x400.jpg" alt="" />
+      <h1>Erlich Bachman</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit culpa facere dicta neque, aspernatur officia, dignissimos harum deleniti iste quod perferendis obcaecati placeat temporibus. Perferendis consequatur iste temporibus nulla ipsum?
+      Eligendi labore voluptatibus aliquam quod ad ullam nihil maiores maxime sint minus veritatis ut tempore at quia animi, rerum quam eveniet magni. Sed, explicabo. Sequi modi nobis debitis aliquam consequuntur.</p>
+      </div>
+
+      <div className="tabs">
+      <img src="https://i.scdn.co/image/ab67706c0000da84a8f856841e132a9c0f6dff19" alt="" />
+      <h1>Erlich Bachman</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit culpa facere dicta neque, aspernatur officia, dignissimos harum deleniti iste quod perferendis obcaecati placeat temporibus. Perferendis consequatur iste temporibus nulla ipsum?
+      Eligendi labore voluptatibus aliquam quod ad ullam nihil maiores maxime sint minus veritatis ut tempore at quia animi, rerum quam eveniet magni. Sed, explicabo. Sequi modi nobis debitis aliquam consequuntur.</p>
+
+      </div>
+
+     
+      </div>
+
+      </div>
+
+      {/* Page 4 */}
+      <div id="footer">
+        <div className="newsletter">Sign up for our News letter to keep <br/>
+         up-to-date with the latest trends in the market. <br/>
+         {/* Add SendGrID API Here */}
+         <input type="email" placeholder="Sign up using your email here"/> <button id="newsbutton">Submit</button>
+         </div>
+        
+        <div className="socials">
+          <h3 id="findushere">Find us here  </h3>
+          <div id="socialLink"> 
+          <a href="https://www.instagram.com/" target="_blank">Instagram </a>
+          <a href="https://www.facebook.com/" target="_blank">FaceBook </a>
+          <a href="https://www.twitter.com/" target="_blank">Twitter </a>
+          <a href="https://in.linkedin.com/" target="_blank">LinkedIn </a>
+          </div>
+          
+          <br />
+          <h4 id="address">Address <br /></h4>
+          <h6 id="addressLink">
+          03/11, Developer Brain Avenue, <br /> 
+          BTM 2'nd Stage, <br /> 
+          Bengaluru, Karnataka, <br /> 
+          India - 560076 <br /> 
+          </h6>
+          
+          
         </div>
       </div>
     </>
