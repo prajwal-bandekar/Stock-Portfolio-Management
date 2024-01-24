9744f593c75d5e09418c0fd4ed84fd5f26d7939f
Prajwal <prajwalbandekar@gmail.com>
2024-01-24 13:37:05 +0530

    Landing Page section Completed 85%

--- spmui/src/components/LP.jsx
+++ spmui/src/components/LP.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./LP.css";
+import './Button.css'
 // import { Link } from "react-router-dom";
 // import InventoryOutlinedIcon from '@mui/icons-material/InventoryOutlined';
 
@@ -34,7 +35,10 @@ const LP = () => {
           <br />
           
           {/* <Link to="/Signup"></Link> */}
-          <button id="button">Get Started</button> 
+          {/* <button id="button">Get Started</button>  */}
+          <button class="button-86" role="button">Get Started</button>
+
+        
           
         </div>
       </div>
@@ -70,7 +74,7 @@ const LP = () => {
       {/* PAGE 3 */}
       <div id="parentdiv3">
       <div id="header">
-        <h1>Testi<span id="monials">monials</span></h1>
+        <h1>Testimonials</h1>
         <h5>Don't just take our word for it. Hear what our users have to say.</h5>
         </div>
 
@@ -79,27 +83,35 @@ const LP = () => {
       <div className="tabs"> 
       <img src="https://avatars.githubusercontent.com/u/8151169?v=4" alt="" />
       <h1>Erlich Bachman</h1>
+      <h6>Chief Evangelist Officer at Pied Piper, Ex Aviato</h6>
       <p>"InvestMitra, the ultimate game changer for managing 
-            my stock portfolio, delivers real-time updates and custom alerts – hands down,
-            the best damn portfolio tracker  I've ever used. I've got skin in the tech game, 
-            owning a solid 10% of Pied Piper. And let's not forget, I have made strategic 
-            moves like selling Aviato in the end. Highly recommend it because missing a 
+            my stock portfolio, Now offcourse, I don't need to keep every of my investments written on my hand.
+             I've got skin in the tech game, owning a solid 10% of Pied Piper. And let's not forget, I have made strategic 
+            moves like selling Aviato in the end, based on my very good track record, I Highly recommend this to everyone, because missing a 
             market move is so last season!"</p>
       <p></p>
       </div>
 
       <div className="tabs">
       <img src="https://pbs.twimg.com/profile_images/731951677859868674/2iRz4Ikp_400x400.jpg" alt="" />
-      <h1>Erlich Bachman</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit culpa facere dicta neque, aspernatur officia, dignissimos harum deleniti iste quod perferendis obcaecati placeat temporibus. Perferendis consequatur iste temporibus nulla ipsum?
-      Eligendi labore voluptatibus aliquam quod ad ullam nihil maiores maxime sint minus veritatis ut tempore at quia animi, rerum quam eveniet magni. Sed, explicabo. Sequi modi nobis debitis aliquam consequuntur.</p>
+      <h1>Gavin Belson</h1>
+      <h6>Investor, Ex CEO Hooli</h6>
+      <p>"An unparalleled masterpiece in the realm of stock portfolio management! InvestMitra,
+         With its cutting-edge features, I can effortlessly gauge how much ground I need to cover 
+         in stock performance to outshine even the likes of Jeff Bezos. The features are nothing 
+         short of revolutionary – a Hooli-level advantage in navigating the intricate landscape of investments.It is offcourse still in question
+         whether it is Hooli property or not as it's still contested in court.."</p>
       </div>
 
       <div className="tabs">
       <img src="https://i.scdn.co/image/ab67706c0000da84a8f856841e132a9c0f6dff19" alt="" />
-      <h1>Erlich Bachman</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit culpa facere dicta neque, aspernatur officia, dignissimos harum deleniti iste quod perferendis obcaecati placeat temporibus. Perferendis consequatur iste temporibus nulla ipsum?
-      Eligendi labore voluptatibus aliquam quod ad ullam nihil maiores maxime sint minus veritatis ut tempore at quia animi, rerum quam eveniet magni. Sed, explicabo. Sequi modi nobis debitis aliquam consequuntur.</p>
+      <h1>Rus Hanneman</h1>
+      <h6>Member of the three comma club and Investor</h6>
+      <p>People, let me tell you, this app InvestMitra, is the Elon Musk of portfolio trackers!
+         It's like having a rocket strapped to my investments - real-time updates, custom alerts
+          that hit harder than a SpaceX launch, and my tres comma club? Oh, it's thriving! 
+          Thanks to InvestMitra, I'm not just tracking stocks; I'm cruising in my latest ride 
+          This app is not just a tool; it's the fuel for my financial journey. Get on the rocket ship, or you'll be left behind!</p>
 
       </div>
 
@@ -119,10 +131,10 @@ const LP = () => {
         <div className="socials">
           <h3 id="findushere">Find us here  </h3>
           <div id="socialLink"> 
-          <a href="https://www.instagram.com/" target="_blank">Instagram </a>
-          <a href="https://www.facebook.com/" target="_blank">FaceBook </a>
-          <a href="https://www.twitter.com/" target="_blank">Twitter </a>
-          <a href="https://in.linkedin.com/" target="_blank">LinkedIn </a>
+          <a href="https://www.instagram.com/" target="_blank">Instagram</a>
+          <a href="https://www.facebook.com/" target="_blank">FaceBook</a>
+          <a href="https://www.twitter.com/" target="_blank">Twitter</a>
+          <a href="https://in.linkedin.com/" target="_blank">LinkedIn</a>
           </div>
           
           <br />
